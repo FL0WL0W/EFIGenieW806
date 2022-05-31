72dca4855237c69f8a652f630f3db7e4c55133e0
FL0WL0W <FL0WL0W@Cracked-Ice.com>
2022-05-30 20:19:03 -0500

    update libs

--- src/main.c
+++ src/main.c
@@ -1,24 +1,8 @@
-/******************************************************************************
-** 
- * \file        main.c
- * \author      IOsetting | iosetting@outlook.com
- * \date        
- * \brief       Demo code of PWM in independent mode
- * \note        This will drive 3 on-board LEDs to show fade effect
- * \version     v0.1
- * \ingroup     demo
- * \remarks     test-board: HLK-W806-KIT-V1.0
- *              PWM Frequency = 40MHz / Prescaler / (Period + 1)；
-                Duty Cycle(Edge Aligned)   = (Pulse + 1) / (Period + 1)
-                Duty Cycle(Center Aligned) = (2 * Pulse + 1) / (2 * (Period + 1))
- *
-******************************************************************************/
-
-#include <stdio.h>
-#include "wm_hal.h"
 #include "Entry.h"
+#include "wm_regs.h"
 
-void Error_Handler() { while (1); }
+#define RCC ((RCC_TypeDef *)RCC_BASE)
+
 //undefined function so defining it
 size_t write(int fd, const void *buf, size_t count)
 {
@@ -27,8 +11,9 @@ size_t write(int fd, const void *buf, size_t count)
 
 int main(void)
 {
-    SystemClock_Config(CPU_CLK_240M);
-    printf("enter main\r\n");
+    RCC->CLK_EN &= ~0x3FFFFF;
+    RCC->BBP_CLK = 0x0F;
+    RCC->CLK_DIV = 0x83060302;
 
     Setup();
 
@@ -36,9 +21,4 @@ int main(void)
     {
         Loop();
     }
-}
-
-void assert_failed(uint8_t *file, uint32_t line)
-{
-    printf("Wrong parameters value: file %s on line %d\r\n", file, line);
 }
