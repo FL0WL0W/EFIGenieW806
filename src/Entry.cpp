13d3ce54b7faff3b9c729519da5f736db91efe5f
FL0WL0W <FL0WL0W@Cracked-Ice.com>
2022-06-01 10:01:54 -0500

    add get variable handler

--- src/Entry.cpp
+++ src/Entry.cpp
@@ -1,32 +1,53 @@
 #include "EFIGenieMain.h"
 #include "EmbeddedIOServiceCollection.h"
 #include "TimerService_W806.h"
+#include "CommunicationService_W806UART.h"
 #include "AnalogService_W806.h"
 #include "DigitalService_W806.h"
 #include "PwmService_W806.h"
 #include "DigitalService_W806.h"
+#include "CommunicationHandlers/CommunicationHandler_GetVariable.h"
+#include "Variable.h"
+#include "Config.h"
 
 using namespace EFIGenie;
 using namespace EmbeddedIOServices;
 using namespace EmbeddedIOOperations;
+using namespace OperationArchitecture;
 
 // extern char _config;
 extern "C"
 {
     EmbeddedIOServiceCollection _embeddedIOServiceCollection;
     EFIGenieMain *_engineMain;
+    CommunicationService_W806UART *_uartService;
+    CommunicationHandler_GetVariable *_getVariableHandler;
+    tick_t prev;
     Task *ledTask;
     Task *led2Task;
     Task *ledPWMTask;
     tick_t ledInterval;
     tick_t led2Interval;
     tick_t ledPWMInterval;
+    Variable *loopTime;
     #define DUTY_MAX 70
     #define DUTY_MIN 0
     int i, j, m[3] = {0}, d[3] = {DUTY_MIN, (DUTY_MIN + DUTY_MAX) / 2, DUTY_MAX - 1};
     void Setup() 
     {
+        _uartService = CommunicationService_W806UART::Create(0, 1024, 115200, UART_WORDLENGTH_8B, UART_STOPBITS_1, UART_PARITY_NONE);
+
+        const char doneResponseText[10] = " (Done)\n\r";
+        const char responseText1[34] = "Initializing EmbeddedIOServices";
+        _uartService->Send((uint8_t*)responseText1, strlen(responseText1));
+        _uartService->Flush();
         _embeddedIOServiceCollection.DigitalService = new DigitalService_W806();
+        _embeddedIOServiceCollection.AnalogService = new AnalogService_W806();
+        _embeddedIOServiceCollection.TimerService = new TimerService_W806(0,1);
+        _embeddedIOServiceCollection.PwmService = new PwmService_W806();
+        _uartService->Send((uint8_t*)doneResponseText, strlen(doneResponseText));
+        _uartService->Flush();
+        
         _embeddedIOServiceCollection.DigitalService->InitPin(34, Out);
         _embeddedIOServiceCollection.DigitalService->AttachInterrupt(0, [](){
             _embeddedIOServiceCollection.DigitalService->WritePin(34, _embeddedIOServiceCollection.DigitalService->ReadPin(0));
@@ -34,8 +55,6 @@ extern "C"
         _embeddedIOServiceCollection.DigitalService->WritePin(34, _embeddedIOServiceCollection.DigitalService->ReadPin(0));
         // _embeddedIOServiceCollection.DigitalService->InitPin(32, Out);
         _embeddedIOServiceCollection.DigitalService->InitPin(33, Out);
-        _embeddedIOServiceCollection.AnalogService = new AnalogService_W806();
-        _embeddedIOServiceCollection.TimerService = new TimerService_W806(0,1);
         ledInterval = _embeddedIOServiceCollection.TimerService->GetTicksPerSecond();
         led2Interval = ledInterval - 100;
         ledPWMInterval = 20001;
@@ -73,13 +92,29 @@ extern "C"
         while(ledTask->Scheduled);
         _embeddedIOServiceCollection.TimerService->ScheduleTask(led2Task, ledTask->ExecutedTick-500000);
         _embeddedIOServiceCollection.TimerService->ScheduleTask(ledPWMTask, ledTask->ExecutedTick+ledPWMInterval);
-        _embeddedIOServiceCollection.PwmService = new PwmService_W806();
         _embeddedIOServiceCollection.PwmService->InitPin(32, Out, 1000);
         // _embeddedIOServiceCollection.PwmService->InitPin(33, Out, 1000);
         // _embeddedIOServiceCollection.PwmService->InitPin(34, Out, 1000);
-        // char nullconf[50] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
-		// size_t _configSize = 0;
-        // _engineMain = new EFIGenieMain(reinterpret_cast<void*>(nullconf), _configSize, &_embeddedIOServiceCollection);
+
+        char nullconf[50] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+		size_t _configSize = 0;
+        const char responseText3[26] = "Initializing EngineMain";
+        _uartService->Send((uint8_t*)responseText3, strlen(responseText3));
+        _uartService->Flush();
+        _engineMain = new EFIGenieMain(reinterpret_cast<void*>(nullconf), _configSize, &_embeddedIOServiceCollection);
+        _uartService->Send((uint8_t*)doneResponseText, strlen(doneResponseText));
+        _uartService->Flush();
+        
+        const void *metadata = nullconf;
+        _getVariableHandler = new CommunicationHandler_GetVariable(_uartService, _engineMain->VariableMap, metadata);
+        
+        const char responseText5[24] = "Setting Up EngineMain";
+        _uartService->Send((uint8_t*)responseText5, strlen(responseText5));
+        _uartService->Flush();
+        // _engineMain->Setup();
+        _uartService->Send((uint8_t*)doneResponseText, strlen(doneResponseText));
+        _uartService->Flush();
+        loopTime = _engineMain->VariableMap->GenerateValue(250);
     }
     void Loop() 
     {
@@ -89,6 +124,11 @@ extern "C"
             _embeddedIOServiceCollection.TimerService->ScheduleTask(led2Task, led2Task->ScheduledTick + led2Interval);
         if(!ledPWMTask->Scheduled)
             _embeddedIOServiceCollection.TimerService->ScheduleTask(ledPWMTask, ledPWMTask->ScheduledTick + ledPWMInterval);
+            
+        const tick_t now = _embeddedIOServiceCollection.TimerService->GetTick();
+        loopTime->Set((float)(now-prev) / _embeddedIOServiceCollection.TimerService->GetTicksPerSecond());
+        prev = now;
+        _uartService->Flush();
         // _engineMain->Loop();
     }
 }
