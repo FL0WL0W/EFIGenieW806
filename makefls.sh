2606325ba17ca2b7f2a5c6bb4943df187ef512bb
FL0WL0W <FL0WL0W@Cracked-Ice.com>
2024-05-30 22:01:06 -0500

    build w806 as dependency instead of submodule

--- makefls.sh
+++ makefls.sh
@@ -1,3 +1,3 @@
-gcc ./wm-sdk-w806/tools/W806/wm_tool.c -lpthread -o ./wm-sdk-w806/tools/W806/wm_tool
-./wm-sdk-w806/tools/W806/wm_tool -b ./build/firmware.bin -fc 0 -it 0 -ih 8002000 -ra 8002400 -ua 8002000 -nh 0 -un 0 -o ./build/firmware
+gcc ./build/W806-Release/_deps/wm-sdk-w806-src/tools/W806/wm_tool.c -lpthread -o ./wm_tool
+./wm_tool -b ./build/firmware.bin -fc 0 -it 0 -ih 8002000 -ra 8002400 -ua 8002000 -nh 0 -un 0 -o ./build/firmware
 cat ./build/firmware.img > ./build/firmware.fls
